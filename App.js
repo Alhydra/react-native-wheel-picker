07cdb708d2251eb85ac3ffb8f279a5a851dd3ff8
Youssef El habchi <ellhydra1@gmail.com>
2020-12-20 15:55:12 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 
-export default function App() {
+import Picker from '@gregfrench/react-native-wheel-picker'
+var PickerItem = Picker.Item;
+
+const App = () => {
+  const [selectedItem, setSelectedItem ] = useState(2);
+  const [itemList , setItemList ] = useState(['Apple', 'Orange', 'Peach', 'Strawberries', 'Pineapple']);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Picker style={{width: 150, height: 180}}
+          lineColor="#000000" 
+          lineGradientColorFrom="#008000" 
+          lineGradientColorTo="#FF5733" 
+          selectedValue={selectedItem}
+          itemStyle={{color:"white", fontSize:26}}
+          onValueChange={(index) => setSelectedItem(index) }>
+          {itemList.map((value, i) => (
+            <PickerItem label={value} value={i} key={i} />
+          ))}
+        </Picker>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex:1,
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center',
+    backgroundColor: '#ff4646'
+  }
+})
